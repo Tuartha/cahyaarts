544eb0b09ec1e76cdf678e9a9f234dd1c6e39101
Gun4rta <gunartajaya2007@gmail.com>
2025-11-05 08:08:50 +0800

    Framework Installation

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./resources/**/*.blade.php",
+    "./resources/**/*.js",
+    "./resources/**/*.vue",
+    "./resources/views/frontend/**/*.blade.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
